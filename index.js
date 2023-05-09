56d619fc474b578cc570a08189da059fcc169fda
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-05-09 16:52:10 -0700

    Add built-in middleware function in Express.js that allows users to serve static files such as HTML, CSS, JavaScript, and images from a directory on our server.

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+//Renders the index page
 app.get('/', (req,res) => {
     res.render('index');
+});
+
+//Renders the main page
+app.get('/main', (req,res) => {
+    res.render('main');
 });
 
 app.listen(port, () => {
     console.log("Node application listening on port " + port);
 });
+
+//Serves the static assets from the specified directory
+app.use(express.static(__dirname + '/public'));
