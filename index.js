856e2bbe0ba4027bd7bc7419114b7af6525f8738
Munyoung Cho <munyoungc31@gmail.com>
2023-05-09 19:13:51 -0700

    Add funtion to link the editProfile page

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req,res) => {
     res.render('index');
 });
 
+app.get('/editProfile', (req,res) => {
+    res.render('editProfile');
+});
+
 app.listen(port, () => {
     console.log("Node application listening on port " + port);
 });
