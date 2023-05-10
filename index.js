427fb884e6547edc57cad03f42531a3dd9dcd339
Munyoung Cho <munyoungc31@gmail.com>
2023-05-09 17:13:18 -0700

    Add the function for rendering the profile page

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', (req,res) => {
     res.render('index');
 });
 
+app.get('/profile', (req,res) => {
+    res.render('profile');
+});
+
 app.listen(port, () => {
     console.log("Node application listening on port " + port);
 });
