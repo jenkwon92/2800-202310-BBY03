8a6e154ad97ec46adca88af3a9a1482e1c509157
Marvin Sio <marvinbcit@gmail.com>
2023-05-10 18:23:30 -0700

    Added user authentication and signup functionality through database integration.

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ require("./utils.js");
 require('dotenv').config();
 
 const express = require('express');
+
+// For storing session information in the database
 const session = require('express-session');
+
+// For storing session information in the database
 const MongoStore = require('connect-mongo');
+
+// bcrypt for password hashing
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
@@ -12,6 +18,11 @@ const port = process.env.PORT || 3000;
 
 /* Create the express app */
 const app = express();
+
+const Joi = require("joi");
+
+//Set expiration time for session to 1 hour
+const expireTime = 1 * 60 * 60 * 1000;
 
 /* Secret Information Section */
 const mongodb_host = process.env.MONGODB_HOST;
@@ -35,20 +46,39 @@ var mongoStore = MongoStore.create({
 var { database } = require("./databaseConnection");
 const userCollection = database.db(mongodb_database).collection("users");
 
-/* Set the static folder */
+app.use(express.urlencoded({ extended: false }));
+
+/* Set the ejs view engine */
+app.set('view engine', 'ejs');
+
+/* Sets the session */
 app.use(session({
     secret: node_session_secret,
     store: mongoStore,
     saveUninitialized: false,
     resave: true,
+    cookie: {
+        maxAge: 3600000 // set session expiration time to 1 hour
+    }
 }));
 
-/* Set the ejs view engine */
-var users = [];
+//Check if the session is valid
+function isValidSession(req) {
+    if (req.session.authenticated) {
+        return true;
+    }
+    return false;
+}
 
-app.use(express.urlencoded({ extended: false }));
-
-app.set('view engine', 'ejs');
+//Check if the session is valid
+function sessionValidation(req, res, next) {
+    if (isValidSession(req)) {
+        next();
+    }
+    else {
+        res.redirect('/login');
+    }
+}
 
 /* Home Section */
 
@@ -59,80 +89,119 @@ app.get('/', (req, res) => {
 
 //Renders the main page
 //When the user logged in
-app.get('/main', (req,res) => {
+app.get('/main', (req, res) => {
     res.render('main');
 });
 
-app.get('/editProfile', (req,res) => {
+app.get('/editProfile', (req, res) => {
     res.render('editProfile');
 });
 
-app.get('/profile', (req,res) => {
+app.get('/profile', (req, res) => {
     res.render('profile');
 });
 
-/* Login Section */
-app.get('/users', async (req, res) => {
-    const users = await userCollection.find().toArray();
-    res.render('users', { users });
-});
-
-// For developers to test on their local machine
+// Renders the signup page
 app.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-app.post('/submitUser', (req, res) => {
-    var email = req.body.email;
-    var username = req.body.username;
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var password = req.body.password;
+// Creates a new user
+app.post('/submitUser', async (req, res) => {
+    const { email, username, firstName, lastName, password } = req.body;
 
-    var hashedPassword = bcrypt.hashSync(password, saltRounds);
-    users.push({ email: email, username: username, firstName: firstName, lastName: lastName, password: hashedPassword });
+    // Check if user with the same email or username already exists
+    const existingUser = await userCollection.findOne({
+        $or: [{ email: email }, { username: username }]
+    });
 
-    console.log(users);
-
-    var usershtml = "";
-    for (i = 0; i < users.length; i++) {
-        usershtml += "<li>" + "<br></br>" +
-            "Email: " + users[i].email + "<br></br>" +
-            "Username: " + users[i].username + "<br></br>" +
-            "First Name: " + users[i].firstName + "<br></br>" +
-            "Last Name: " + users[i].lastName + "<br></br>" +
-            "Password: " + users[i].password + "<br></br>" +
-            "</li>";
+    // If user already exists, return error message
+    if (existingUser) {
+        if (existingUser.email === email) {
+            return res.send("User with this email already exists");
+        } else {
+            return res.send("User with this username already exists");
+        }
     }
 
-    var html = "<ul>" + usershtml + "</ul>";
-    res.send(html);
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Create new user
+    const newUser = {
+        email: email,
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        password: hashedPassword
+    };
+
+    // Insert new user into database
+    await userCollection.insertOne(newUser);
+    // Console log to show that user was added
+    console.log("User added to database");
+
+    // Set session variables
+    req.session.authenticated = true;
+    req.session.username = username;
+
+    // Redirect to main page after signup
+    res.redirect("/main");
 });
 
+// Renders the login page
 app.get('/login', (req, res) => {
+
+    // Show error message if there is one
     var msg = req.query.msg || '';
 
     res.render("login", { msg: msg })
 });
 
-app.post('/submitLogin', (req,res) => {
-    var email = req.body.email;
-    var password = req.body.password;
+// Logs the user in
+app.post('/submitLogin', async (req, res) => {
+    const { email, password } = req.body;
 
-    var usershtml = "";
-    for (i = 0; i < users.length; i++) {
-        if (users[i].email == email) {
-            if (bcrypt.compareSync(password, users[i].password)) {
-                res.redirect('/loggedIn');
-                return;
-            }
-        }
+    // Checks for valid email
+    const schema = Joi.string().required();
+    const validationResult = schema.validate(email);
+
+    // If email is invalid, return error message
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect('/login?msg=Invalid Email/Password!');
+        return;
     }
 
-    //User and Password combination not found
-    res.redirect('/login?msg=Invalid Email/Password!');
+    const user = await userCollection.findOne({ email: email });
+
+    // If email does not exist, return error message
+    if (!user) {
+        console.log("Email not found");
+        res.redirect('/login?msg=Invalid Email!');
+        return;
+    }
+
+    // Checks if the password matches using bcrypt compare
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    // If password does not match, return error message
+    if (!passwordMatch) {
+        console.log("Incorrect password");
+        res.redirect('/login?msg=Invalid Password!');
+        return;
+    }
+
+    // Set session variables
+    req.session.authenticated = true;
+    req.session.username = user.username;
+    req.session.email = email;
+    req.session.cookie.maxAge = expireTime;
+
+    res.redirect('/main');
 });
 
+// For developers to test on their local machine
 app.listen(port, () => {
     console.log("Node application listening on port " + port);
 });
