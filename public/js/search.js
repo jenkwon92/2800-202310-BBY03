00a65bf8fd62675463850a674c5536f37fc60dc2
Marvin Sio <marvinbcit@gmail.com>
2023-05-23 22:44:43 -0700

    Moved functions to an external js file

---
+++ public/js/search.js
@@ -0,0 +1,17 @@
+function populateSearch(tag) {
+    const searchInput = document.getElementById('search');
+    searchInput.value = tag;
+    const searchForm = document.getElementById('search-form');
+    searchForm.submit();
+}
+
+function goToPage(page) {
+    const searchInput = document.getElementById('search');
+    const searchForm = document.getElementById('search-form');
+    const pageInput = document.createElement('input');
+    pageInput.type = 'hidden';
+    pageInput.name = 'page';
+    pageInput.value = page;
+    searchForm.appendChild(pageInput);
+    searchForm.submit();
+}
