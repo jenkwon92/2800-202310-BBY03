fb4d788a48c2c6ee59b4039765aafaa434cb00ee
Yongeun Kwon <ykwon35@my.bcit.ca>
2023-05-25 01:57:31 -0700

    Added course bookmark remove function

--- public/js/courseDetail.js
+++ public/js/courseDetail.js
@@ -1,19 +1,49 @@
 function saveCourse() {
-  var button = document.getElementById('course_id');     // Get the courseId from the server-side template
+  var button = document.getElementById('course_id');
   console.log(button);
   const courseId = button.getAttribute('data');
-  const xhr = new XMLHttpRequest();                         // Create a new XMLHttpRequest object
-  xhr.open('POST', '/saveCourse');                          // Set up the POST request to the '/saveCourse' endpoint
-  xhr.setRequestHeader('Content-Type', 'application/json'); // Set the request header for JSON content
-  xhr.onload = function () {                                // Define the onload event handler for the request
-    if (xhr.status === 200) {                               // Check if the response status is 200 (success)
-      console.log('The course saved successfully');         // Log a success message
-      //window.location.href = '/myCourses';                // Redirect to the 'myCourses' page
-
-      swal("Success!", "Course has been saved!", "success");
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/saveCourse');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log('The course removal was successful');
+      // Display success message using sweetalert library
+      swal({
+        title: "Success!",
+        text: "Course has been saved!",
+        icon: "success",
+      }).then(() => {
+        window.location.href = '/myCourses';
+      });
     } else {
-      console.error('Error saving a course');               // Log an error message
+      console.error('Error saving a course');
     }
   };
-  xhr.send(JSON.stringify({ courseId: courseId }));         // Send the courseId as JSON data in the request body
+  xhr.send(JSON.stringify({ courseId: courseId }));
+}
+
+function removeCourse() {
+  var button = document.getElementById('course_id');
+  console.log(button);
+  const courseId = button.getAttribute('data');
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/removeCourse');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log('The course removal was successful');
+      // Display success message using sweetalert library
+      swal({
+        title: "Success!",
+        text: "Course has been removed!",
+        icon: "success",
+      }).then(() => {
+        window.location.href = '/myCourses';
+      });
+    } else {
+      console.error('Error removing the course');
+    }
+  };
+  xhr.send(JSON.stringify({ courseId: courseId }));
 }
